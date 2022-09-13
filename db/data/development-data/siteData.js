a0c03aacbcdf3bc715f33973cc1cab68bc8e353c
callumu21 <cmu_92@hotmail.co.uk>
2022-09-13 15:26:51 +0100

    Add more detail to the sites and tours in the development database

--- db/data/development-data/siteData.js
+++ db/data/development-data/siteData.js
@@ -3,7 +3,8 @@ module.exports = seedSites = [
     siteId: 1,
     authorId: 1,
     siteName: "Durham Cathedral",
-    siteDescription: "This is Durham Cathedral",
+    siteDescription:
+      "The Cathedral Church of Christ, Blessed Mary the Virgin and St Cuthbert of Durham, commonly known as Durham Cathedral and home of the Shrine of St Cuthbert, is a cathedral in the city of Durham, England. It is the seat of the Bishop of Durham, the fourth-ranked bishop in the Church of England hierarchy.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/durham_cathedral.jpg",
     siteAddress: "Durham DH1 3EH",
@@ -16,7 +17,8 @@ module.exports = seedSites = [
     siteId: 2,
     authorId: 1,
     siteName: "Durham Castle",
-    siteDescription: "This is Durham Castle",
+    siteDescription:
+      "Durham Castle is a Norman castle in the city of Durham, England, which has been occupied since 1837 by University College, Durham after its previous role as the residence of the Bishops of Durham.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/durham_castle.jpg",
     siteAddress: "Durham DH1 3RW",
@@ -30,7 +32,8 @@ module.exports = seedSites = [
     siteId: 3,
     authorId: 1,
     siteName: "Botanic Garden",
-    siteDescription: "This is Durham Botanic Garden",
+    siteDescription:
+      "The Durham University Botanic Garden is a botanical garden located in Durham, England. The site is set in 25 acres of mature woodlands in the southern outskirts of the city.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/botanic_garden.jpeg",
     siteAddress: "Durham DH1 3DF",
@@ -43,7 +46,8 @@ module.exports = seedSites = [
     siteId: 4,
     authorId: 1,
     siteName: "Durham University",
-    siteDescription: "This is Durham University",
+    siteDescription:
+      "Durham University is a collegiate public research university in Durham, England, founded by an Act of Parliament in 1832 and incorporated by royal charter in 1837.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/durham_university.jpg",
     siteAddress: "Durham",
@@ -56,7 +60,8 @@ module.exports = seedSites = [
     siteId: 5,
     authorId: 1,
     siteName: "Wollaton Hall",
-    siteDescription: "This is Wollaton Hall",
+    siteDescription:
+      "Wollaton Hall is an Elizabethan country house of the 1580s standing on a small but prominent hill in Wollaton Park, Nottingham, England. The house is now Nottingham Natural History Museum, with Nottingham Industrial Museum in the outbuildings.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/wollaton_hall.jpg",
     siteAddress: "Nottingham NG8 2AE",
@@ -69,7 +74,8 @@ module.exports = seedSites = [
     siteId: 6,
     authorId: 1,
     siteName: "City of Caves",
-    siteDescription: "This is the City of Caves",
+    siteDescription:
+      "City of Caves is a visitor attraction in Nottingham based on a network of caves, carved out of sandstone that have been variously used over the years as a tannery, public house cellars, and as an air raid shelter.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/city_of_caves.jpg",
     siteAddress: "Garner's Hill, Nottingham NG1 1HF",
@@ -82,7 +88,8 @@ module.exports = seedSites = [
     siteId: 7,
     authorId: 1,
     siteName: "The Arboretum",
-    siteDescription: "This is The Arboretum",
+    siteDescription:
+      "The Arboretum was the first designated public park in Nottingham, selected under the authority of the Inclosure Act 1845. The botanist and horticultural publisher, Samuel Curtis, oversaw the design of the park. The park officially opened on 11 May 1852, and was declared Grade II listed with Historic England in 1986.",
     siteImage: "https://myguideimages.s3.eu-west-2.amazonaws.com/arboretum.jpg",
     siteAddress: "Waverley St, Nottingham NG7 4HF",
     latitude: 52.961235998594105,
@@ -95,7 +102,8 @@ module.exports = seedSites = [
     siteId: 8,
     authorId: 1,
     siteName: "Nottingham Industrial Museum",
-    siteDescription: "This is Nottingham Industrial Museum",
+    siteDescription:
+      "The Nottingham Industrial Museum is a volunteer-run museum situated in part of the 17th-century stables block of Wollaton Hall, located in a suburb of the city of Nottingham. The museum won the Nottinghamshire Heritage Site of the Year Award 2012, a local accolade issued by Experience Nottinghamshire.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/nottingham_industrial_museum.jpg",
     siteAddress:
@@ -109,7 +117,8 @@ module.exports = seedSites = [
     siteId: 9,
     authorId: 2,
     siteName: "Yorkshire Wildlife Park",
-    siteDescription: "This is Yorkshire Wildlife Park",
+    siteDescription:
+      "The Yorkshire Wildlife Park, or known as simply Yorkshire Zoo, is a wildlife conservation, rehabilition centre, 300-acre zoo, and tourist attraction located in Branton, south-east of Doncaster, South Yorkshire, England.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/yorkshire_wildlife_park.jpg",
     siteAddress: "Hurst Ln, Auckley, Doncaster DN9 3HQ",
@@ -136,7 +145,8 @@ module.exports = seedSites = [
     siteId: 11,
     authorId: 2,
     siteName: "Brodsworth Hall and Gardens",
-    siteDescription: "This is Brodsworth Hall and Gardens",
+    siteDescription:
+      "Brodsworth Hall, near Brodsworth, 5 miles north-west of Doncaster in South Yorkshire, is one of the most complete surviving examples of a Victorian country house in England. It is virtually unchanged since the 1860s.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/brodsworth_hall.jpg",
     siteAddress: "Brodsworth, Doncaster DN5 7XJ",
@@ -150,7 +160,8 @@ module.exports = seedSites = [
     siteId: 12,
     authorId: 2,
     siteName: "Doncaster Racecourse",
-    siteDescription: "This is Doncaster Racecourse",
+    siteDescription:
+      "Doncaster Racecourse is a racecourse in Doncaster, South Yorkshire, England. It hosts two of Great Britain's 36 annual Group 1 flat races, the St Leger Stakes and the Racing Post Trophy.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/doncaster_racecourse.jpg",
     siteAddress: "Bawtry Rd, Doncaster DN2 6BB",
@@ -163,7 +174,8 @@ module.exports = seedSites = [
     siteId: 13,
     authorId: 3,
     siteName: "Manchester Art Gallery",
-    siteDescription: "This is Manchester Art Gallery",
+    siteDescription:
+      "Manchester Art Gallery, formerly Manchester City Art Gallery, is a publicly owned art museum on Mosley Street in Manchester city centre. The main gallery premises were built for a learned society in 1823 and today its collection occupies three connected buildings, two of which were designed by Sir Charles Barry.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/manchester_art_gallery.jpg",
     siteAddress: "Manchester Art Gallery, Mosley St, Manchester M2 3JL",
@@ -176,7 +188,8 @@ module.exports = seedSites = [
     siteId: 14,
     authorId: 3,
     siteName: "Science and Industry Museum",
-    siteDescription: "This is the Science and Industry Museum",
+    siteDescription:
+      "The Science and Industry Museum in Manchester, England, traces the development of science, technology and industry with emphasis on the city's achievements in these fields.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/science_and_industry_museum.jpeg",
     siteAddress: "Liverpool Rd, Manchester M3 4FP",
@@ -189,7 +202,8 @@ module.exports = seedSites = [
     siteId: 15,
     authorId: 3,
     siteName: "People's History Museum",
-    siteDescription: "This is the People's History Museum",
+    siteDescription:
+      "The People's History Museum in Manchester, England, is the UK's national centre for the collection, conservation, interpretation and study of material relating to the history of working people in the UK.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/peoples_history_museum.jpg",
     siteAddress: "Left Bank, Manchester M3 3ER",
@@ -202,7 +216,8 @@ module.exports = seedSites = [
     siteId: 16,
     authorId: 3,
     siteName: "National Football Museum",
-    siteDescription: "This is the National Football Museum",
+    siteDescription:
+      "The National Football Museum is England's national museum of football. It is based in the Urbis building in Manchester city centre, and preserves, conserves and displays important collections of football memorabilia. The museum was originally based in Deepdale, Preston, Lancashire, but moved to Manchester in 2012.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/national-football-museum.jpg",
     siteAddress: "Urbis Building Cathedral Gardens, Todd St, Manchester M4 3BG",
@@ -215,7 +230,8 @@ module.exports = seedSites = [
     siteId: 17,
     authorId: 3,
     siteName: "Manchester Museum",
-    siteDescription: "This is Manchester Museum",
+    siteDescription:
+      "Manchester Museum is a museum displaying works of archaeology, anthropology and natural history and is owned by the University of Manchester, in England. Sited on Oxford Road at the heart of the university's group of neo-Gothic buildings, it provides access to about 4.5 million items from every continent.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/manchester_museum.jpg",
     siteAddress: "University of, Oxford Rd, Manchester M13 9PL",
@@ -228,7 +244,8 @@ module.exports = seedSites = [
     siteId: 18,
     authorId: 3,
     siteName: "Manchester Jewish Museum",
-    siteDescription: "This is Manchester Jewish Museum",
+    siteDescription:
+      "Manchester Jewish Museum occupies the former Spanish and Portuguese synagogue on Cheetham Hill Road in Manchester, England. It is a grade II listed building.",
     siteImage:
       "https://myguideimages.s3.eu-west-2.amazonaws.com/manchester_jewish_museum.jpg",
     siteAddress: "190 Cheetham Hill Rd, Cheetham Hill, Manchester M8 8LW",
