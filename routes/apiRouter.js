5b44bce5e72654740ab516a7ca841465b536a0cf
Jason Efstathiou <Jason.r.e@hotmail.com>
2022-09-13 11:55:54 +0100

    Finished the endpoints-json file with all of the endpoints and example responses for each one

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,9 +1,14 @@
 const apiRouter = require("express").Router();
 const sitesRouter = require("./sitesRouter");
 const toursRouter = require("./toursRouter");
+const endpoints = require("../endpoints.json");
 
 apiRouter.use("/sites", sitesRouter);
 
 apiRouter.use("/tours", toursRouter);
+
+apiRouter.route("/").get((req, res, next) => {
+  res.status(200).send(endpoints);
+});
 
 module.exports = apiRouter;
